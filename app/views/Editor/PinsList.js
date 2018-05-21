8aaafe9b073ce1f02890e83f744b574abec26b23
Joseph Thomas <joseph@good-idea.studio>
2018-05-21 15:43:35 -0700

    Adds pin list to editor

--- app/views/Editor/PinsList.js
+++ app/views/Editor/PinsList.js
@@ -2,32 +2,58 @@
 import React from 'react'
 import styled from 'styled-components'
 import type { PinType } from 'App/types'
-import { ListItem, Column } from 'App/components/Layout'
-import { spacing } from 'App/styles/layout'
+import { Header2 } from 'App/components/Text'
+import { spacing, boxStyles } from 'App/styles/layout'
+import { middleGray } from 'App/styles/colors'
 
-const PinsListWrapper = styled.aside`
-	position: absolute;
-	top: 0;
-	left: 0;
-	max-height: 100%;
-	overflow-y: scroll;
-	padding: ${spacing.double};
+/**
+ * PinListItem
+ */
+
+const PinListItemWrapper = styled.aside`
+	margin-bottom: ${spacing.single};
+	> span {
+		color: ${middleGray};
+	}
 `
+
+type PinListProps = {
+	pin: PinType,
+}
+
+const PinListItem = ({ pin }: PinListProps) => (
+	<PinListItemWrapper>
+		<p>{pin.title}</p>
+		<h4>
+			{pin.lat}, {pin.lang}
+		</h4>
+	</PinListItemWrapper>
+)
 
 /**
  * PinsList
  */
+
+const PinsListWrapper = styled.aside`
+	position: absolute;
+	z-index: 100;
+	top: ${spacing.single};
+	left: ${spacing.single};
+	max-height: calc(100% - ${spacing.double});
+	overflow-y: scroll;
+	padding: ${spacing.single};
+	${boxStyles};
+`
 
 type Props = {
 	pins: Array<PinType>,
 }
 
 const PinsList = ({ pins }: Props) => {
-	console.log(pins)
-	return null
 	return (
 		<PinsListWrapper>
-			<Column>{pins.map((pin) => <ListItem title={pin.title} line1={`${pin.lat}, ${pin.lang}`} />)}</Column>
+			<Header2>Your Pins</Header2>
+			{pins.map((pin) => <PinListItem pin={pin} />)}
 		</PinsListWrapper>
 	)
 }
