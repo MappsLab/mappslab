78b6c0231c8862a61643fbf23de783382dd08afc
Joseph Thomas <joseph@good-idea.studio>
2018-08-10 12:33:30 -0700

    Updates deployment config

--- scripts/deploy-changes.sh
+++ scripts/deploy-changes.sh
@@ -9,25 +9,25 @@ if [ -z ${TRAVIS_BUILD_DIR+x} ]
 fi
 
 
-# # if git diff --name-only $TRAVIS_COMMIT_RANGE | grep "^api/"
-# 	# then 
-# 	echo "*     * * *     *"
-# 	echo "🚀 Deploying API"
-# 	echo "*     * * *     *"
-# 	cd $TRAVIS_BUILD_DIR
+# if git diff --name-only $TRAVIS_COMMIT_RANGE | grep "^api/"
+	# then 
+	echo "*     * * *     *"
+	echo "🚀 Deploying API"
+	echo "*     * * *     *"
+	cd $TRAVIS_BUILD_DIR
 
-# 	# if [ "$TRAVIS" == true ]
-# 		# then
-# 		cd ./build/api
-# 		echo "🛠  Deploying to now"
-# 		now -e 
-# 		# now -e --token $NOW_TOKEN
-# 		echo "🛠  Creating alias"
-# 		now alias 
-# 		# now alias --token $NOW_TOKEN
-# 		echo "🛠  Build complete! 🎉"
-# # 	fi
-# # fi
+	# if [ "$TRAVIS" == true ]
+		# then
+		cd ./build/api
+		echo "🛠  Deploying to now"
+		now -e 
+		# now -e --token $NOW_TOKEN
+		echo "🛠  Creating alias"
+		now alias 
+		# now alias --token $NOW_TOKEN
+		echo "🛠  Build complete! 🎉"
+# 	fi
+# fi
 
 
 
