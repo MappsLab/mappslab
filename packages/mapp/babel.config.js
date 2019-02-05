fde5b63bc43e7106920c68b9b4355a25691d2832
Joseph Thomas <joseph@good-idea.studio>
2019-02-04 17:26:27 -0700

    fix(mapp): remove flowtypes in babel

--- packages/mapp/babel.config.js
+++ packages/mapp/babel.config.js
@@ -1,9 +1,9 @@
-const path = require('path')
+// const path = require('path')
 
 module.exports = {
 	presets: ['@babel/preset-flow', '@babel/preset-env', '@babel/preset-react'],
 	plugins: [
-		'flow-react-proptypes',
+		// 'flow-react-proptypes',
 		'@babel/plugin-proposal-class-properties',
 		'@babel/plugin-proposal-object-rest-spread',
 		'react-hot-loader/babel',
