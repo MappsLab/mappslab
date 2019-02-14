776134cad17b7237ee3d6b8abc134855f67a85a4
Joseph Thomas <joseph@good-idea.studio>
2019-02-13 23:20:05 -0800

    fix(app): update graphql endpoints

--- app/src/config.js
+++ app/src/config.js
@@ -1,7 +1,7 @@
 // @flow
 
-const apiRoot = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://mappslab-api.now.sh'
-const wsUri = process.env.NODE_ENV === 'development' ? 'ws://localhost:3000' : 'wss://mappslab-api.now.sh'
+const apiRoot = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/graphql' : 'https://mappslab-api.now.sh/graphql'
+const wsUri = process.env.NODE_ENV === 'development' ? 'ws://localhost:3000/graphql' : 'wss://mappslab-api.now.sh/graphql'
 
 export default {
 	apiRoot,
