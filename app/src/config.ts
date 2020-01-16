95aa0a0ab00de1dd38fa2eb1f5235342e8b434e7
Joseph Thomas <joseph@good-idea.studio>
2020-01-15 21:42:13 -0500

    fix(app): update staging api websocket url

--- app/src/config.ts
+++ app/src/config.ts
@@ -5,7 +5,7 @@ const apiRoot =
 const wsUri =
 	process.env.NODE_ENV === 'development'
 		? 'ws://localhost:3000/graphql'
-		: 'wss://mappslab-api.now.sh/graphql'
+		: 'wss://mappslab-beta-api.good-idea.studio/graphql'
 const imageBucketRoot =
 	process.env.AWS_BUCKET_ROOT || process.env.NODE_ENV === 'development'
 		? 'http://mappslab-beta.localhost:4572/'
@@ -19,11 +19,6 @@ const dataLayerRoot =
 	process.env.AWS_BUCKET_ROOT || process.env.NODE_ENV === 'development'
 		? 'http://mappslab-beta.localhost:4572/'
 		: 'https://mappslab-beta.s3.amazonaws.com/'
-
-/** Staging */
-// const apiRoot = 'https://mappslab-api-staging.now.sh/graphql'
-// const wsUri = 'wss://mappslab-api-staging.now.sh/graphql'
-// const imageBucketRoot = 'https://mappslab-beta.s3.amazonaws.com/'
 
 export default {
 	apiRoot,
